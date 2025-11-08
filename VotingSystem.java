8f9abea73048b2514d7cd01d3c9126086bdb7af5
Dev-ruzirex <dev.ruzirex@gmail.com>
2025-11-08 20:10:52 +0100

    Add files via upload

---
+++ VotingSystem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class VotingSystem {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter your name: ");
+    String name = input.nextLine();
+    System.out.print("Enter your age: ");
+
+    int age = input.nextInt();
+
+if (age >= 18) {
+    System.out.print("You are eligible to vote");
+}
+
+else {
+    System.out.print("You are not eligible to vote");
+    }
+
+}
+}
